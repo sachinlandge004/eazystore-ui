284d7bba8b6e41fb18e597543f8627d2ec392793
Sachin Landge <sachinlandge004@gmail.com>
2025-08-15 14:56:05 +0530

    added backend connectivity

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,24 @@
 import PageHeading from "./PageHeading";
-import products from "../data/products";
 import ProductListings from "./ProductListings";
+import apiClient from "../api/apiClient"; 
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetchProducts();
+  },[])
+
+  const fetchProducts = async () => {
+    try {
+      const response = await apiClient.get("/products");
+      setProducts(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="home-container">
       <PageHeading title="Expolre Eazy Stickers!">
