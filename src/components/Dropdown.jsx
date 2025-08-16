773330f08966e28d7667821a452473506765ed44
Sachin Landge <sachinlandge004@gmail.com>
2025-08-16 12:27:10 +0530

    search functionality implemented

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Dropdown({ label, options, selectedValue }) {
+  return (
+    <div className="flex items-center gap-2 justify-end pr-12 flex-1 font-primary">
+      <label className="text-lg font-semibold text-primary dark:text-light">
+        {label}
+      </label>
+      <select
+        className="px-3 py-2 text-base border rounded-md transition border-primary dark:border-light focus:ring focus:ring-dark  dark:focus:ring-lighter focus:outline-none text-gray-800 dark:text-lighter"
+        value={selectedValue} 
+      >
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
--- src/components/ProductListings.jsx
+++ src/components/ProductListings.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductCard from "./ProductCard";
+import SearchBox from "./SearchBox";
+import Dropdown from "./Dropdown";
+
+const sortList = ["Popularity", "Price Low to High", "Price High to Low"];
 
 export default function ProductListings({ products }) {
+
+  const [searchText, setSearchText] = useState('');// handles changes in input search
+
+  function handleSearchChange(inputSearch) {
+    setSearchText(inputSearch);
+    console.log(inputSearch);
+
+  }
+
+  let filteredAndSortedProducts = products.filter((product) => {
+    return product.name.toLowerCase().includes(searchText.toLowerCase()) ||
+           product.description.toLowerCase().includes(searchText.toLowerCase());
+  });
+
   return (
     <div className="product-listings-container">
+      <div className="flex flex-col sm:flex-row justify-between items-center gap-4 pt-12">
+        <SearchBox
+          label="Search  "
+          placeholder="Search products..."
+          value={searchText}
+          handleSearchChange={(value) => handleSearchChange(value)}
+        />
+        <Dropdown
+          label="Sort by  "
+          options={sortList}
+        />
+      </div>
       <div className="product-listings-grid">
-        {products.length > 0 ? (
-          products.map((product) => (
+        {filteredAndSortedProducts.length > 0 ? (
+          filteredAndSortedProducts.map((product) => (
             <ProductCard key={product.productId} product={product} />
           ))
         ) : (
